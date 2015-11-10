7223e982be97cda7af9e543d25b80cc3d022a995
mateja683 <mateja683@gmail.com>
2015-11-10 12:28:03 +0000

    finishing javabuzz

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -13,7 +13,37 @@ Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
 };
 
 Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByFifteen(number)) {
+    return "Javabuzz";
+  }
   if (this.isDivisibleByThree(number)) {
     return "Java";
   }
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  }
+  return number;
 };
+
+Array.range= function(a, b, step){
+    var A= [];
+    if(typeof a== 'number'){
+        A[0]= a;
+        step= step || 1;
+        while(a+step<= b){
+            A[A.length]= a+= step;
+        }
+    }
+    else{
+        var s= 'abcdefghijklmnopqrstuvwxyz';
+        if(a=== a.toUpperCase()){
+            b=b.toUpperCase();
+            s= s.toUpperCase();
+        }
+        s= s.substring(s.indexOf(a), s.indexOf(b)+ 1);
+        A= s.split('');
+    }
+    return A;
+};
+
+console.log(javabuzz.says(Array.range(0,100)));
